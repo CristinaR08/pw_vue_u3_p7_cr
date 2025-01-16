085a7116846d1943f45be82ecf331dcf89a7361b
CristinaR08 <cris.04082009@hotmail.com>
2025-01-16 00:49:27 +0000

    Taller 21: Vue con API

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>Adivina el pokemon de la imagen</h2>
+  <Imagen />
+  <Opciones />
+</template>
+
+<script>
+import Imagen from '@/components/PokemonImagen.vue'
+import Opciones from '@/components/PokemonOpciones.vue'
+
+export default {
+  components: {
+    Imagen,
+    Opciones
+  }
+    
+}
+</script>
+
+<style>
+
+</style>
