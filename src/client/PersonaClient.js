297e6888e4ddc2e7034bd4074d352906cb97dbe7
CristinaR08 <cris.04082009@hotmail.com>
2025-02-14 16:39:14 -0500

    Tarea 13: Completar formulario front

--- src/client/PersonaClient.js
+++ src/client/PersonaClient.js
@@ -1,15 +1,49 @@
 
 import axios from 'axios';
 
-const obtenerPorId = (id) => {
-    //http://localhost:8080/matriculaAPI/v1.1/personas
-    axios.get(`http://localhost:8080/matriculaAPI/v1.1/personas/${id}`).then(r => r.data);
-    console.log(data);
-    return data;
+const obtenerPorId = async (id) => {
+    const response = await axios.get(`http://localhost:8080/matriculaAPI/v1.1/personas/${id}`);
+    return response.data;
 }
 
-export const obtenerPersonaIDFachada = async(id) => {
+// const crearPersona = async (persona) => {
+//     const response = await axios.post('http://localhost:8080/matriculaAPI/v1.1/personas', persona);
+//     return response.data;
+// }
+
+// const actualizarPersona = async (id, persona) => {
+//     const response = await axios.put(`http://localhost:8080/matriculaAPI/v1.1/personas/${id}`, persona);
+//     return response.data;
+// }
+
+// const modificarPersona = async (id, persona) => {
+//     const response = await axios.patch(`http://localhost:8080/matriculaAPI/v1.1/personas/${id}`, persona);
+//     return response.data;
+// }
+
+// const eliminarPersona = async (id) => {
+//     const response = await axios.delete(`http://localhost:8080/matriculaAPI/v1.1/personas/${id}`);
+//     return response.data;
+// }
+
+export const obtenerPersonaIDFachada = async (id) => {
     return await obtenerPorId(id);
 }
+
+// export const crearPersonaFachada = async (persona) => {
+//     return await crearPersona(persona);
+// }
+
+// export const actualizarPersonaFachada = async (id, persona) => {
+//     return await actualizarPersona(id, persona);
+// }
+
+// export const modificarPersonaFachada = async (id, persona) => {
+//     return await modificarPersona(id, persona);
+// }
+
+// export const eliminarPersonaFachada = async (id) => {
+//     return await eliminarPersona(id);
+// }
 
 
--- src/components/Persona/FormularioComponente.vue
+++ src/components/Persona/FormularioComponente.vue
@@ -1,16 +1,74 @@
 <template>
     <h1>Bienvenidx :3</h1>
+    <div>
+        <form class="formulario">
+            <div class="form-group">
+                <label for="nombre">Nombre:</label>
+                <input type="text" id="nombre" v-model="nombre">
+            </div>
+            <div class="form-group">
+                <label for="apellido">Apellido:</label>
+                <input type="text" id="apellido" v-model="apellido">
+            </div>
+            <div class="form-group">
+                <label for="fechaNacimiento">Fecha de Nacimiento:</label>
+                <input type="date" id="fechaNacimiento" v-model="fechaNacimiento">
+            </div>
+            <button type="submit" @click.prevent="submitForm">Enviar</button>
+        </form>
+    </div>
+
 </template>
 
 <script>
 //import {obtenerPorIdFachada} from "../client/PersonaClient"
 
 export default {
-    
+
 
 }
 </script>
 
 <style>
+.formulario {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: #f9f9f9;
+}
 
+.form-group {
+    margin-bottom: 15px;
+}
+
+.form-group label {
+    display: block;
+    margin-bottom: 5px;
+    font-weight: bold;
+}
+
+.form-group input {
+    width: 100%;
+    padding: 8px;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+
+button {
+    display: block;
+    width: 100%;
+    padding: 10px;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: #0056b3;
+}
 </style>
